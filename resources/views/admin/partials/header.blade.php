42290c8f02a36f8956ac5008c789d71abda91ff0
matteoga95 <gallo.work95@gmail.com>
2023-02-09 09:12:30 +0100

    samll fix header

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -20,9 +20,7 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}
-                    </li>
+
 
                 </ul>
 
