70ee5e581300594e600d55297d571e407da2afec
Gianmarco720 <orabonagianmarco379@gmail.com>
2023-02-13 15:53:11 +0100

    add paginate number

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
         return response()->json([
             'status' => 'success',
 
-            'data' => Restaurant::with(['tipologies', 'dishes'])->orderByDesc('id')->paginate()
+            'data' => Restaurant::with(['tipologies', 'dishes'])->orderByDesc('id')->paginate(6)
         ]);
     }
 
